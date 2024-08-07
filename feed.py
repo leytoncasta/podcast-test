117c392fcbcdc9518c8b360cf04c3d22073ff69d
Yhon L. Castañeda <35607553+leytoncasta@users.noreply.github.com>
2024-08-07 02:49:40 +0000

    fixing feed.py bug

--- feed.py
+++ feed.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as xml_tree
 
 with open('feed.yaml', 'r') as file:
     yaml_data = yaml.safe_load(file)
-    rss_element=xml_tree.Element('rss', {'rss version':'2.0',
+    rss_element=xml_tree.Element('rss', {'version':'2.0',
         'xmlns:itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd',
         'xmlns:content':'http://purl.org/rss/1.0/modules/content/'})
 
